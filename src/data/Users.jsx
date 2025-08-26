dce6153064daa6821c5a8b38c886d23b0a948a5a
dan-seng <da16gi@gmail.com>
2025-08-26 16:16:05 +0300

    Created users for login page

---
+++ src/data/Users.jsx
@@ -0,0 +1,24 @@
+const users = [
+  {
+    name: "Daniel Gidey",
+    email: "da16gi@gmail.com",
+    password: "daniel123"
+  },
+  {
+    name: "MJ H",
+    email: "mjspid@gmail.com",
+    password: "sarah456"
+  },
+  {
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: "john789"
+  },
+  {
+    name:"Hamelmal Gidey", 
+    email:"hamgid@gmail.com",
+    password:"21232123"
+  }
+];
+
+export default users;
